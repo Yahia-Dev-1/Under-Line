3c188fc763901e00d6366ac211dba6b5cf66ab91
iiyahia <yahiapro400@gmail.com>
2026-01-30 13:17:51 +0200

    Fix vite proxy target port to 5501 and syntax error

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5500',
+        target: 'http://localhost:5501',
         changeOrigin: true,
         secure: false
       }
